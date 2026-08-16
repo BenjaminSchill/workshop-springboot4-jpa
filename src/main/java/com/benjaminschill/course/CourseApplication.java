a5cb7a35b28d1f1b1abb3d74b214c1fe1f52e171
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-16 12:48:14 -0300

    User entity and resource

--- src/main/java/com/benjaminschill/course/CourseApplication.java
+++ src/main/java/com/benjaminschill/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.Benjamin_Schill.course;
+package com.benjaminschill.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
