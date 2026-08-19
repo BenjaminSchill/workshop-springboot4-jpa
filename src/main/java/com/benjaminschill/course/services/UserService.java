58b5d44de88cd48c0968e7e4096a3e35b67a2dc2
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-19 12:08:50 -0300

    Exception handling - findById

--- src/main/java/com/benjaminschill/course/services/UserService.java
+++ src/main/java/com/benjaminschill/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.benjaminschill.course.entities.User;
 import com.benjaminschill.course.repositories.UserRepository;
+import com.benjaminschill.course.services.Exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) { 
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) { 
