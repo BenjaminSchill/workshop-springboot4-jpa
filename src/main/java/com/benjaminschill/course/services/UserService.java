660a38c764455f94c4a56e1351fbaf10eeb70b51
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-19 20:09:51 -0300

    Exception handling - update

--- src/main/java/com/benjaminschill/course/services/UserService.java
+++ src/main/java/com/benjaminschill/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.benjaminschill.course.repositories.UserRepository;
 import com.benjaminschill.course.services.Exceptions.DatabaseException;
 import com.benjaminschill.course.services.Exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -48,9 +50,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) { 
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try { 
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) { 
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
